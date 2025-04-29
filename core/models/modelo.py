00362fab4cb59d669eb2aae3d8cec9e54295fea6
fernandapadilha <vitoriafernandaifc@gmail.com>
2025-04-29 17:55:28 -0300

    feat: Criação da API de modelo

---
+++ core/models/modelo.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Modelo(models.Model):
+    nome = models.CharField(max_length=80)
+    marca = models.CharField(max_length=80, null=True, blank=True)
+    categoria = models.CharField(max_length=80, null=True, blank=True)
+
+    def _str_(self):
+        return f"id: {self.id}; marca: {self.marca}; nome: {self.nome}"
