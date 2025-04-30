5130a36f6aa315e4bbf3ca67d7cae960376a4b09
fernandapadilha <vitoriafernandaifc@gmail.com>
2025-04-30 11:27:08 -0300

    fix: Corrreção da model de veículo

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+from .acessorio import Acessorio
+from .cor import Cor
+from .modelo import Modelo
+
 
 class Veiculo(models.Model):
-    ano = models.IntegerField(default=0, null=True)
-    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
-   
-    
+    ano = models.IntegerField(null=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
+    modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE)
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE)
+    acessorios = models.ManyToManyField(Acessorio, blank=True)
+
     def __str__(self):
-        return f"id: {self.id};"
+        return f"id: {self.id}; modelo: {self.modelo}; cor: {self.cor}; ano: {self.ano}"
